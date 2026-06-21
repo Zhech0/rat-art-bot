2f361085f44362cf2b509c142c116ab711bb2489
Zhech0 <yungbeanznrice@gmail.com>
2026-06-21 00:00:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const http = require('http');
+http.createServer((req, res) => res.end('Bot alive')).listen(process.env.PORT || 3000);
+
 const { Client } = require('@fluxerjs/core');
 const client = new Client();
 const TOKEN = process.env.TOKEN
 client.login(TOKEN);
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
