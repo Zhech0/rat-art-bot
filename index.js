3ce9f1f51cb1e5e4325df31b6d6312225788d29b
Zhech0 <yungbeanznrice@gmail.com>
2026-06-20 22:54:40 -0700

    Log token length and starting characters
    Added logging for token length and starting characters.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client } = require('@fluxerjs/core');
 const client = new Client();
 const TOKEN = process.env
-
+console.log('Token length:', TOKEN? TOKEN.length: 'undefined');
+console.log('Token starts with:', TOKEN? TOKEN.substring(0, 5): 'undefined');
 client.login(TOKEN);
