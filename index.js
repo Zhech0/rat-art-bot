f2c35f6b17322ecaee891d6f760baff90c0549d6
Zhech0 <yungbeanznrice@gmail.com>
2026-06-20 23:10:12 -0700

    Remove console logs and call client.login

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const { Client } = require('@fluxerjs/core');
 const client = new Client();
 const TOKEN = 1518038615047806976.XbHyyRzyhjlzbiu7X6iKPo_Dm4lIKRI2Z0QFxIFa2SM.env
-console.log('Token length:', TOKEN? TOKEN.length: 'undefined');
-console.log('Token starts with:', TOKEN? TOKEN.substring(0, 5): 'undefined');
-console.log('Available env vars:', Object.keys(process.env));
 client.login(TOKEN);
