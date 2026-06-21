670079de2e5ce5d04f330ccd2b678f1a24c6fd60
Zhech0 <yungbeanznrice@gmail.com>
2026-06-20 22:58:55 -0700

    Log available environment variables

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const client = new Client();
 const TOKEN = process.env
 console.log('Token length:', TOKEN? TOKEN.length: 'undefined');
 console.log('Token starts with:', TOKEN? TOKEN.substring(0, 5): 'undefined');
+console.log('Available env vars:', Object.keys(process.env));
 client.login(TOKEN);
