62a8b910551b420c4906b2a14cd16c30fee80534
Zhech0 <yungbeanznrice@gmail.com>
2026-06-20 23:13:07 -0700

    Shorten TOKEN variable in index.js
    Updated TOKEN variable to a shortened version.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { Client } = require('@fluxerjs/core');
 const client = new Client();
-const TOKEN = 1518038615047806976.XbHyyRzyhjlzbiu7X6iKPo_Dm4lIKRI2Z0QFxIFa2SM.env
+const TOKEN = 151803861
 client.login(TOKEN);
