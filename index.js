61ad7244eaae3e9f741227acbdb15237ab2cbbf6
Zhech0 <yungbeanznrice@gmail.com>
2026-06-20 23:06:17 -0700

    Fix token assignment from environment variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client } = require('@fluxerjs/core');
 const client = new Client();
-const 1518038615047806976.XbHyyRzyhjlzbiu7X6iKPo_Dm4lIKRI2Z0QFxIFa2SM = process.env
+const TOKEN=1518038615047806976.XbHyyRzyhjlzbiu7X6iKPo_Dm4lIKRI2Z0QFxIFa2SM.env
 console.log('Token length:', TOKEN? TOKEN.length: 'undefined');
 console.log('Token starts with:', TOKEN? TOKEN.substring(0, 5): 'undefined');
 console.log('Available env vars:', Object.keys(process.env));
