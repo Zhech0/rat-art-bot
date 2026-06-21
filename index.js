50bf34e87756673d85c8ee8eb11233694130624f
Zhech0 <yungbeanznrice@gmail.com>
2026-06-20 23:21:30 -0700

    Use environment variable for login token
    Replace hardcoded token with environment variable.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { Client } = require('@fluxerjs/core');
 const client = new Client();
-const TOKEN = 1518038615047806976.KNaY-ezjz-yqtEYjCgQqK7llTkYbBbtblrRVPzsLPmY
+const TOKEN = process.env
 client.login(TOKEN);
