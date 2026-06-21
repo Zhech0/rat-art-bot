2f91ade04051b5dc57e781680b50ebf9a1c78c8d
Zhech0 <yungbeanznrice@gmail.com>
2026-06-20 23:17:16 -0700

    Change TOKEN to new login token
    Updated the TOKEN variable to use a new value.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { Client } = require('@fluxerjs/core');
 const client = new Client();
-const TOKEN = 151803861
+const TOKEN = 1518038615047806976.XbHyyRzyhjlzbiu7X6iKPo_Dm4lIKRI2Z0QFxIFa2SM
 client.login(TOKEN);
